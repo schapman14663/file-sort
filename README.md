a77c0b924943054f9180fff5a0d85b9917ceea79
Simon Chapman <schapman14663@gmail.com>
2026-09-19 10:44:00 +0100

    start of project

---
+++ README.md
@@ -0,0 +1,3 @@
+# FILE SORT
+A short program to sort files from my downloads folder into other folders in my home directory. 
+
---
+++ src/file_sort/__init__.py
@@ -0,0 +1,21 @@
+#I think this might need import os
+
+def main() -> None:
+    print("Hello from file-sort!")
+
+    downloads = #insert downloads directory path here
+
+    extensions = [
+        ".txt",
+        ".epub",
+        ".pdf",
+        ".iso",
+        ".jpeg",
+        ".mpeg",
+    ]
+
+    #pattern should be a filename split at the dot?
+    #then moved to *relevant directory*
+    for file in downloads:
+        case pattern:
+            pass
